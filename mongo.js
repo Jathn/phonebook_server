de486d6a20667d1322ef19bf09e4ddf7aba654e6
Jathn <jonathan.ramstedt@gmail.com>
2024-01-16 06:37:46 +0200

    Task 3.12 done

--- mongo.js
+++ mongo.js
@@ -6,26 +6,40 @@ if (process.argv.length<3) {
 }
 
 const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
 
 const url =
-  `mongodb+srv://fullstack:${password}@cluster0.yp5guiq.mongodb.net/?retryWrites=true&w=majority`
+  `mongodb+srv://jonathanramstedt:${password}@cluster0.yp5guiq.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+const personSchema = new mongoose.Schema({
+  id: Number,
+  name: String,
+  number: String,
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Person = mongoose.model('Person', personSchema)
 
-const note = new Note({
-  content: 'HTML is Easy',
-  important: true,
+const person = new Person({
+  id: 5,
+  name: name,
+  number: number,
 })
 
-note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
-})
+if (name === undefined || number === undefined) {
+  Person.find({}).then(result => {
+    console.log('Phonebook:')
+    result.forEach(person => {
+      console.log(person.name + ' ' + person.number)
+    })
+    mongoose.connection.close()
+  })
+} else {
+  person.save().then(result => {
+    console.log('Added ' + name + ' number ' + number + ' to phonebook')
+    mongoose.connection.close()
+  })  
+}
